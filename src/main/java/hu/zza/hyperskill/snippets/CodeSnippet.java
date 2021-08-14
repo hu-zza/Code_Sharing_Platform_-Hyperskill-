6d2830a2eb8e00ee3a317e4dcdb31f7034fec69f
Szabó László András <noreply@zza.hu>
2021-08-14 23:01:48 +0200

    Add test data to DB, Bootstrap CSS & JS, static CSS

--- src/main/java/hu/zza/hyperskill/snippets/CodeSnippet.java
+++ src/main/java/hu/zza/hyperskill/snippets/CodeSnippet.java
@@ -7,6 +7,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -24,6 +25,7 @@ public class CodeSnippet {
   private long viewCount = 0L;
 
   // JSON attributes
+  @Column(columnDefinition = "text")
   private String code = "";
   private LocalDateTime date = LocalDateTime.now();
 
